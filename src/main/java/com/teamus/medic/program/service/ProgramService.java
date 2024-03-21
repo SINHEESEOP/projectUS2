1f1c8cc7372cf25868ba5cfa41a560a9ba0f5c6d
kohyeonjin <guswlsdl729@gmail.com>
2024-03-21 17:11:04 +0900

    asdf

--- src/main/java/com/teamus/medic/program/service/ProgramService.java
+++ src/main/java/com/teamus/medic/program/service/ProgramService.java
@@ -5,19 +5,24 @@ import java.util.List;
 import com.teamus.medic.command.ProgramRegVO;
 import com.teamus.medic.command.ProgramVO;
 import com.teamus.medic.command.UserVO;
+import com.teamus.medic.util.Criteria;
 
 public interface ProgramService {
 
 	public int programRegist(ProgramVO vo);
-	public List<ProgramVO> getProgramList();
+	public List<ProgramVO> getProgramList(Criteria cri);
 	public ProgramVO getDetail(int EVNT_NO);
 	public void programUpdate(ProgramVO vo);
 	public void programDelete(int EVNT_NO);
-	public List<ProgramRegVO> getProgramRegList();
+	public List<ProgramRegVO> getProgramRegList(String USERNAME , Criteria Cri);
 	public UserVO getUser(String USER_NO);
-	public void programUserRegist(String USER_NO , int EVNT_NO);
+	public void programUserRegist(String USERNAME , int EVNT_NO);
 	public void deleteApply(int EVNT_REG_NO);
-	public List<ProgramRegVO> getProgramApplyList();
+	public List<ProgramRegVO> getProgramApplyList(Criteria cri);
 	public void applyUpdate(int EVNT_REG_NO);
-	public void successUpdate(int ENVT_NO);
+	public void successUpdate(int EVNT_REG_NO);
+	public int getProgramTotal();
+	public int getProgramApplyTotal(Criteria cri);
+	public int getUserApplyTotal(String USERNAME);
+	public int programApplydelete(int EVNT_NO);
 }
