a5209b0811b4246ee6230bef35080fb0232f0d6f
kohyeonjin <guswlsdl729@gmail.com>
2024-03-21 17:57:47 +0900

    dev2

--- src/main/java/com/teamus/medic/controller/EquipAdminController.java
+++ src/main/java/com/teamus/medic/controller/EquipAdminController.java
@@ -1,4 +1,4 @@
-package com.teamus.medic.controller;
+	package com.teamus.medic.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
