1e97d9768abf13ec4ea7cf55efb63dfd7c77f2e4
kohyeonjin <guswlsdl729@gmail.com>
2024-03-22 15:49:43 +0900

    master go

--- src/main/java/com/teamus/medic/controller/MainController.java
+++ src/main/java/com/teamus/medic/controller/MainController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.teamus.medic.User.service.UserMapper;
-import com.teamus.medic.User.service.userService;
 import com.teamus.medic.command.UserVO;
 import com.teamus.medic.securlty.MyUserDetails;
 
